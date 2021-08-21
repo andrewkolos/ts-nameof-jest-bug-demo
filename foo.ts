247c0b43968fb099edf770b8e804e3545825867e
Andrew Kolos <andrewrkolos@gmail.com>
2021-08-21 15:53:32 -0400

    initial commit

---
+++ foo.ts
@@ -0,0 +1,6 @@
+const foo = {
+  baz: '1',
+  bar: '2'
+}
+
+export default foo
---
+++ older-dependencies/test/foo.test.ts
@@ -0,0 +1,7 @@
+import foo from '../../foo';
+
+describe(nameof(foo), () => {
+  it('passes', () => {
+    expect(true).toBe(true)
+  })
+})
